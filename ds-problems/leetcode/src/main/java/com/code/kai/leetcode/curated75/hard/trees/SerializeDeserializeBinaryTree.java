2197768f55968f7b12c84a8e1d36245b1d28e3d3
Mohan Sharma <mohan.sharma@sap.com>
2022-02-06 11:21:43 +0530

    Day-13 :  Tree problems

---
+++ ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/hard/trees/SerializeDeserializeBinaryTree.java
@@ -0,0 +1,53 @@
+package com.code.kai.leetcode.curated75.hard.trees;
+
+import com.code.kai.leetcode.curated75.easy.trees.TreeNode;
+
+/**
+ * @author Mohan Sharma
+ */
+public class SerializeDeserializeBinaryTree {
+
+    private int index = 0;
+    public String serialize(TreeNode root) {
+        if (root == null)
+            return null;
+        return getPreOrderString(root);
+    }
+
+    private String getPreOrderString(TreeNode root) {
+        if (root == null)
+            return  "X";
+        return root.val + "," + getPreOrderString(root.left) + "," +  getPreOrderString(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null)
+            return null;
+        return buildTreeRecursively(data.split(","));
+    }
+
+    private TreeNode buildTreeRecursively(String[] preorder) {
+        if (index == preorder.length )
+            return null;
+        String rootVal = preorder[index++];
+        if (rootVal.equals("X"))
+            return null;
+        TreeNode root = new TreeNode(Integer.parseInt(rootVal));
+        root.left = buildTreeRecursively(preorder);
+        root.right = buildTreeRecursively(preorder);
+        return root;
+    }
+
+    public  void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        TreeNode r = new TreeNode(3);
+        r.left = new TreeNode(4);
+        r.right = new TreeNode(5);
+        root.right = r;
+        String serialize = serialize(root);
+        System.out.println(serialize);
+        System.out.println(deserialize(serialize));
+    }
+}
