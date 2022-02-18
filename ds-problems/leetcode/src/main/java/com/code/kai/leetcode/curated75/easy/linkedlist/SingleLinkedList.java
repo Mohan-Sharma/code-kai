3a95d73cf11768bb6b4b730ea4667635835419fc
Mohan Sharma <mohan.sharma@sap.com>
2022-02-18 20:56:13 +0530

    Day-20 :  LinkedList problems

--- ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/easy/linkedlist/SingleLinkedList.java
+++ ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/easy/linkedlist/SingleLinkedList.java
@@ -36,6 +36,37 @@ public class SingleLinkedList {
         return previous;
     }
 
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //creating dummy node so dont need to check if head of result is null first time
+        ListNode result = new ListNode(0);
+        //current will always point to tail for next insert whereas result will store the complete merged list
+        ListNode current = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        current.next = list1 == null ? list2 : list1;
+        //ignore the first 0 node
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList l1 = new SingleLinkedList();
+        l1.insertNode(1);
+        l1.insertNode(4);
+        l1.insertNode(5);
+        SingleLinkedList l2 = new SingleLinkedList();
+        l2.insertNode(1);
+        l2.insertNode(3);
+        l2.insertNode(4);
+        mergeTwoLists(l1.head, l2.head);
+    }
 }
 
 class ListNode {
