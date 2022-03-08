6fe809432c4657e1b8d2cff2f71d0e8ddb0cc552
Mohan Sharma <mohan.sharma@sap.com>
2022-03-08 10:40:23 +0530

    find max function added

--- ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/easy/arrays/SortedRotatedArray.java
+++ ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/easy/arrays/SortedRotatedArray.java
@@ -25,6 +25,27 @@ public class SortedRotatedArray {
         return result;
     }
 
+    public static int findMax(int[] nums) {
+        int result = -1;
+        if (nums.length == 1)
+            return nums[0];
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            if (nums[start] <= nums[end]) {
+                result = nums[end];
+                break;
+            }
+            int mid =  (start + end)/2;
+            if (nums[mid] > nums[end]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(findMin(new int[] {2, 1}));
     }
