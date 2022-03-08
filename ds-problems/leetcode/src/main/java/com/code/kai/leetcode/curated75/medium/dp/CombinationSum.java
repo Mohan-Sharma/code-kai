4e3bd1a8362d62c1390c29f2852f7a3491016629
Mohan Sharma <mohan.sharma@sap.com>
2022-03-08 10:40:00 +0530

    replace hashmap to array

--- ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/medium/dp/CombinationSum.java
+++ ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/medium/dp/CombinationSum.java
@@ -1,46 +1,42 @@
 package com.code.kai.leetcode.curated75.medium.dp;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Mohan Sharma
  */
 public class CombinationSum {
     public static int combinationSum4(int[] nums, int target) {
-        return combinationSumBottomUp(nums, target, new HashMap<>());
+        return combinationSumBottomUp(nums, target, new int[target + 1]);
     }
 
-    private static int combinationSumBottomUp(int[] nums, int target, Map<Integer, Integer> dp ) {
-        dp.put(0, 1);
-        for (int i = 1; i < target + 1; i++) {
-            int value = 0;
+    private static int combinationSumBottomUp(int[] nums, int target, int[] dp ) {
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
             for (int num : nums) {
-                final int key = i - num;
-                value  += dp.getOrDefault(key, 0);
+                if (i - num >= 0) {
+                    dp[i] = dp[i] + dp[i - num];
+                }
             }
-            dp.put(i, value);
         }
-        return dp.get(target);
+        return dp[target];
     }
 
-    private static int combinationSumTopDown(int[] nums, int target, Map<Integer, Integer> dp ) {
+    private static int combinationSumTopDown(int[] nums, int target, int[] dp) {
         int count = 0;
         if (target == 0) {
             return 1;
         }
-        if (dp.containsKey(target))
-            return dp.get(target);
+        if (dp[target] > 0)
+            return dp[target];
         for (int num : nums) {
             if (target - num >= 0) {
                 count += combinationSumTopDown(nums, target-num, dp);
-                dp.put(target, count);
+                dp[target] = count;
             }
         }
         return count;
     }
 
     public static void main(String[] args) {
-        System.out.println(combinationSum4(new int[]{1,2,3}, 4));
+        System.out.println(combinationSum4(new int[]{1, 2, 3}, 4));
     }
 }
