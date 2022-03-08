1cdef271450792928f6845ffd652b96f76362fe3
Mohan Sharma <mohan.sharma@sap.com>
2022-03-08 10:39:24 +0530

    Adding more explanation

--- ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/easy/linkedlist/DetectCycle.java
+++ ds-problems/leetcode/src/main/java/com/code/kai/leetcode/curated75/easy/linkedlist/DetectCycle.java
@@ -4,6 +4,8 @@ package com.code.kai.leetcode.curated75.easy.linkedlist;
  * @author Mohan Sharma
  */
 public class DetectCycle {
+    // explanation of why moving the slow to head works to find the start of the loop
+    //https://stackoverflow.com/questions/1536944/detecting-the-start-of-a-loop-in-a-singly-linked-link-list
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
